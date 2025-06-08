6442b3e17f5400ca9d0fa71991ce18348eef8bfa
Udav-94 <ups-fti@yandex.ru>
2025-06-08 21:35:19 +0300

    Правки и тест

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React, {useState} from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
 import {Menu} from '../Menu/Menu.jsx';
 import {PrimaryHeder} from '../PrimaryHeder/PrimaryHeder.jsx';
@@ -13,64 +13,76 @@ import {StartComponent} from '../StartComponent/StartComponent.jsx';
 import {Workplace} from '../Workplace/Workplace.jsx';
 import {LastMessagePlace} from '../LastMessagePlace/LastMessagePlace.jsx';
 
-function HomePage() {
-  const aboutUsRef = useRef(null);
-  const howItWorksRef = useRef(null);
-  const reviewsRef = useRef(null);
-  const footerRef = useRef(null);
-  const location = useLocation();
-
-  React.useEffect(() => {
-    if (location.state?.scrollTo) {
-      const ref = {
-        'about-us': aboutUsRef,
-        'how-it-works': howItWorksRef,
-        'reviews': reviewsRef,
-        'footer': footerRef
-      }[location.state.scrollTo];
-      
-      if (ref && ref.current) {
-        ref.current.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [location]);
-
-  return (
-    <div>
-      <Menu />
-      <PrimaryHeder />
-      <StartComponent type='folded' />
-      <div ref={aboutUsRef}><AboutUs /></div>
-      <div ref={howItWorksRef}><HowItWorks /></div>
-      <div ref={reviewsRef}><Reviews /></div>
-      <div ref={footerRef}><Footer /></div>
-    </div>
-  );
-}
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/ticket-page' element={
-          <div>
-            <Menu /> 
-            <OperationHeader />
-            <StartComponent type='horizontal'/>
-            <Workplace />
-            <Footer />
-          </div>
-        } />
-        <Route path='/last-page' element={
-          <div>
-            <Menu />
-            <LastHeader />
-            <LastMessagePlace /> 
-            <Footer />
-          </div>
-        } />
-      </Routes>
-    </Router>
-  );
+
+
+    return (
+        <Router>
+        <Routes>
+           <Route path='/' element={
+            <div>
+                <Menu />
+                <PrimaryHeder />
+                <StartComponent type = 'folded' />
+                <AboutUs />
+                <HowItWorks />
+                <Reviews />
+                <Footer />
+            </div>
+           } />
+            <Route path='/about-us' element={ <div><Menu /><AboutUs /></div>}/>
+            <Route path='/how-it-works' element={<div><Menu /><HowItWorks /></div>}/>
+            <Route path='/reviews' element={<div><Menu /><Reviews /></div>}/>
+            <Route path='/footer' element={<div><Menu /><Footer /></div>}/>
+            <Route path='/ticket-page' element={
+            <div>
+                <Menu /> 
+                <OperationHeader />
+                <StartComponent type = 'horizontal'/>
+                <Workplace />
+                <Footer />
+            </div>
+           } />
+            <Route path='/last-page' element={
+            <div>
+                <Menu />
+                <LastHeader />
+                <LastMessagePlace /> 
+                <Footer />
+            </div>
+           } />
+          
+        </Routes>
+      </Router>
+        
+        
+        
+    );
 }
+
+
+
+
+
+
+/* (
+    <Router>
+    <Routes>
+       <Route path='/' element={
+        <div>
+            <Menu />
+            <PrimaryHeder />
+        </div>
+       } />
+        <Route path='/about-us' element={<AboutUs />}/>
+        <Route path='/how-it-works' element={<HowItWorks />}/>
+        <Route path='/reviews' element={<Reviews />}/>
+        <Route path='/footer' element={<Footer />}/>
+      
+    </Routes>
+  </Router>
+    
+    
+    
+); */
